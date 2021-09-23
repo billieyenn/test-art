4b08d3a7fa73af5096cc2998ed636e0c916acfa0
Jaakko Laiho <billie.yenn@gmail.com>
2021-09-23 16:54:54 +0200

    example sketch

--- sketch.js
+++ sketch.js
@@ -9,49 +9,49 @@ return (config) => {
       p.translate(-240, -100, 0);
       p.normalMaterial();
       p.push();
-      p.rotateZ(frameCount * 0.01);
-      p.rotateX(frameCount * 0.01);
-      p.rotateY(frameCount * 0.01);
+      p.rotateZ(p.frameCount * 0.01);
+      p.rotateX(p.frameCount * 0.01);
+      p.rotateY(p.frameCount * 0.01);
       p.plane(70);
       p.pop();
 
       p.translate(240, 0, 0);
       p.push();
-      p.rotateZ(frameCount * 0.01);
-      p.rotateX(frameCount * 0.01);
-      p.rotateY(frameCount * 0.01);
+      p.rotateZ(p.frameCount * 0.01);
+      p.rotateX(p.frameCount * 0.01);
+      p.rotateY(p.frameCount * 0.01);
       p.box(70, 70, 70);
       p.pop();
 
       p.translate(240, 0, 0);
       p.push();
-      p.rotateZ(frameCount * 0.01);
-      p.rotateX(frameCount * 0.01);
-      p.rotateY(frameCount * 0.01);
+      p.rotateZ(p.frameCount * 0.01);
+      p.rotateX(p.frameCount * 0.01);
+      p.rotateY(p.frameCount * 0.01);
       p.cylinder(70, 70);
       p.pop();
 
       p.translate(-240 * 2, 200, 0);
       p.push();
-      p.rotateZ(frameCount * 0.01);
-      p.rotateX(frameCount * 0.01);
-      p.rotateY(frameCount * 0.01);
+      p.rotateZ(p.frameCount * 0.01);
+      p.rotateX(p.frameCount * 0.01);
+      p.rotateY(p.frameCount * 0.01);
       p.cone(70, 70);
       p.pop();
 
       p.translate(240, 0, 0);
       p.push();
-      p.rotateZ(frameCount * 0.01);
-      p.rotateX(frameCount * 0.01);
-      p.rotateY(frameCount * 0.01);
+      p.rotateZ(p.frameCount * 0.01);
+      p.rotateX(p.frameCount * 0.01);
+      p.rotateY(p.frameCount * 0.01);
       p.torus(70, 20);
       p.pop();
 
       p.translate(240, 0, 0);
       p.push();
-      p.rotateZ(frameCount * 0.01);
-      p.rotateX(frameCount * 0.01);
-      p.rotateY(frameCount * 0.01);
+      p.rotateZ(p.frameCount * 0.01);
+      p.rotateX(p.frameCount * 0.01);
+      p.rotateY(p.frameCount * 0.01);
       p.sphere(70);
       p.pop();
     }
